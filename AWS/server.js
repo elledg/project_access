78c9c15e3f2833a3fe925dd743d06d3364e5b8ec
Rome <romejohn.delacruz@gmail.com>
2022-03-23 09:24:05 +0800

    Fixed server JSON inputs

--- AWS/server.js
+++ AWS/server.js
@@ -30,8 +30,15 @@ function addToLog(data) {
 var ctr = 0
 wss.on("connection", function(ws) {
   var id = setInterval(function() {
-    var incident = {trafficID:"AKB48", start:"2021-04-25T10:01:05", stop:"2021-04-25T10:02:35", gps:"15,133,12,110"};
-    var data = {data:[incident, incident]}
+    var inc0 = {trafficID:"AKB00", start:"2021-04-25T10:01:05", stop:"2021-04-25T10:02:35", gps:"15,133,12,110"};
+    var inc1 = {trafficID:"AKB01", start:"2021-04-25T10:01:05", stop:"2021-04-25T10:03:35", gps:"15,133,12,110"};
+    var inc2 = {trafficID:"AKB02", start:"2021-04-25T10:01:05", stop:"2021-04-25T10:02:35", gps:"15,133,12,110"};
+    var inc3 = {trafficID:"AKB03", start:"2021-04-25T10:01:05", stop:"2021-04-25T10:03:35", gps:"15,133,12,110"};
+    var inc4 = {trafficID:"AKB04", start:"2021-04-25T10:01:05", stop:"2021-04-25T10:02:35", gps:"15,133,12,110"};
+    var inc5 = {trafficID:"AKB05", start:"2021-04-25T10:01:05", stop:"2021-04-25T10:04:35", gps:"15,133,12,110"};             
+    var inc6 = {trafficID:"AKB06", start:"2021-04-25T10:01:05", stop:"2021-04-25T10:02:35", gps:"15,133,12,110"};
+    var inc7 = {trafficID:"AKB07", start:"2021-04-25T10:01:05", stop:"2021-04-25T10:04:35", gps:"15,133,12,110"};
+    var inc8 = {trafficID:"AKB08", start:"2021-04-25T10:01:05", stop:"2021-04-25T10:01:35", gps:"15,133,12,110"};
     console.log(data)
     ws.send(JSON.stringify(data), function() {  });
 
